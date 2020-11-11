055b42442b1a01d6d8c5a4ab1771f76fb0511cad
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-11-11 16:51:53 -0500

    agregando validación

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+// Constructores
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+function UI() { }
+
+//llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+        min = max - 20
+
+        const selectYear = document.querySelector("#year");
+
+        for(let i = max; i > min; i--){
+            let option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            selectYear.appendChild(option);
+        }
+}
+
+// instanciar UI
+const Ui = new UI();
+
+document.addEventListener("DOMContentLoaded", () => {
+    Ui.llenarOpciones();
+})
+
+evenListeners();
+function evenListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // leer la marca seleccionada
+    const marca = document.querySelector("#marca").value;
+
+    //leer el año seleccionado
+    const year = document.querySelector("#year").value;
+
+    // leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === "" || year === "" || tipo === ""){
+
+    } else {
+        
+    }
+}
